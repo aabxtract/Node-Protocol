5a213207181d430938ee8c44a573be7c798c7ce3
Hesed Anu Afolami <hesedanu@gmail.com>
2025-10-10 23:02:37 +0000

    The Node protocol logo

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,16 +1,15 @@
-export default function Logo(props: { className?: string }) {
+'use client';
+
+import Image from 'next/image';
+
+export default function Logo({className}: {className?: string}) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
-    </svg>
+    <Image
+      src="/nodeprotocol.png"
+      alt="Node Protocol Logo"
+      width={32}
+      height={32}
+      className={className}
+    />
   );
 }
